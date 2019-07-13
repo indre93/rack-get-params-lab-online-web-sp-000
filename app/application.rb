411d31d334ccb28ece7f447019124efafa3aeb98
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-13 15:46:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,10 +13,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      resp.write "Path Not Found"
     else
       resp.write "Path Not Found"
       resp.write "Your cart is empty"
-
     end
 
     resp.finish
