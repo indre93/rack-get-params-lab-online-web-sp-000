94bf33535c3afceb57781ebb840eba8131d06dfc
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-13 15:54:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |cart_item|
-        resp.write "#{cart_item}"
+        resp.write "#{cart_item}\n"
       end
     else
       resp.write "Path Not Found"
